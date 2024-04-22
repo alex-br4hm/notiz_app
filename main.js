9a1f0300f52e83fc70aa4aa74e88141419020f70
br4hm <haehnlein.alexander@gmx.de>
2024-04-22 19:46:17 +0200

    Initial Commit

---
+++ main.js
@@ -0,0 +1,16 @@
+function addNotice() {
+  let noteHeadline = document.getElementById("noteHeadline").value;
+  let noteText = document.getElementById("noteText").value;
+  let noteContainer = document.getElementById("notesCardContainer");
+
+  noteContainer.innerHTML += `
+    <div class="notes-card-content">
+        <div class="note-headline-card" id="noteHeadlineCard">${noteHeadline}</div>
+     <div class="note-text-card" id="noteTextCard">${noteText}
+    <div class="cards-delete-btn">&#10005;</div>
+    <div class="cards-edit-btn">edit</div>
+    </div>
+    </div>
+    </div>
+    `;
+}
